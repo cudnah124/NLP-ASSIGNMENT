19da68970a55f19476724f8f437d6f7aaec6cac4
cudnah124 <cudnah.124@gmail.com>
2026-05-13 01:20:55 +0700

    init

--- BTL3/src/data_ingestion.py
+++ BTL3/src/data_ingestion.py
@@ -6,8 +6,8 @@ from langchain_huggingface import HuggingFaceEmbeddings
 def run_ingestion():
     print("Starting data ingestion...")
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    clauses_path = os.path.join(base_dir, "..", "BTL1", "output", "clauses.txt")
-    intents_path = os.path.join(base_dir, "..", "BTL2", "output", "intent_classification.txt")
+    clauses_path = os.path.join(base_dir, "data1", "BTL1", "clauses.txt")
+    intents_path = os.path.join(base_dir, "data1", "BTL2", "intent_classification.txt")
     
     # Read data
     clauses = []
